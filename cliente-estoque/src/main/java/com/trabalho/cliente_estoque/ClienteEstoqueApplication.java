12c7ebad95e97f2d29d407a1d49a7967eb59d719
Marcelo Magalhães <128628299+marcelomagalhaes1@users.noreply.github.com>
2025-11-23 18:51:22 -0300

    Add files via upload

---
+++ cliente-estoque/src/main/java/com/trabalho/cliente_estoque/ClienteEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.cliente_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteEstoqueApplication.class, args);
+	}
+
+}
---
+++ cliente-estoque/src/test/java/com/trabalho/cliente_estoque/ClienteEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.cliente_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
