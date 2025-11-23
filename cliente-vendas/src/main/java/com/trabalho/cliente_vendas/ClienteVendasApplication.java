67712ac0b71de523a98e53bdddc43f5c698903df
Marcelo Magalhães <128628299+marcelomagalhaes1@users.noreply.github.com>
2025-11-23 18:51:09 -0300

    Add files via upload

---
+++ cliente-vendas/src/main/java/com/trabalho/cliente_vendas/ClienteVendasApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.cliente_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteVendasApplication.class, args);
+	}
+
+}
---
+++ cliente-vendas/src/test/java/com/trabalho/cliente_vendas/ClienteVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.cliente_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
