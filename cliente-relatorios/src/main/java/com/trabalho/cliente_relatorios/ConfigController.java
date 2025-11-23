66465564105f978ae976f70d152e564c9b034689
Marcelo Magalhães <128628299+marcelomagalhaes1@users.noreply.github.com>
2025-11-23 18:51:35 -0300

    Add files via upload

---
+++ cliente-relatorios/src/main/java/com/trabalho/cliente_relatorios/ConfigController.java
@@ -0,0 +1,17 @@
+package com.trabalho.cliente_relatorios;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${mensagem.boasvindas:Configuração não carregada}")
+    private String mensagem;
+
+    @GetMapping("/mensagem")
+    public String mensagem() {
+        return this.mensagem;
+    }
+}
---
+++ cliente-relatorios/src/test/java/com/trabalho/cliente_relatorios/ClienteRelatoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.cliente_relatorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteRelatoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
